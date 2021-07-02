eaf9222ff12a61710e0a9b869b6b563e8e019d9d
Antonio Ladeia <contato@antonioladeia.com>
2021-07-02 17:10:34 -0300

    minor code improvements, extract method for format text, fiz bug for empty cost

--- tipTime/app/src/main/java/com/antonioladeia/tiptime/MainActivity.kt
+++ tipTime/app/src/main/java/com/antonioladeia/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +20,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
 
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+        if (cost == null) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -31,15 +35,16 @@ class MainActivity : AppCompatActivity() {
 
         var tip = tipPercentage * cost
 
-        val roundUp = binding.roundUpSwitch.isChecked
-
-        if (roundUp) {
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
+        displayTip(tip)
+        
+    }
+
+    private fun displayTip(tip : Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
     }
 }
