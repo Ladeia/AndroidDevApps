65c105545bad0065314ce6a6390620cf054ce85d
Antonio Ladeia <contato@antonioladeia.com>
2021-07-27 07:18:37 -0300

    first version of affirmations showing size of data source in textview

---
+++ Affirmations/app/src/main/java/com/antonioladeia/affirmations/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.antonioladeia.affirmations
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.antonioladeia.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadAffirmations().size.toString()
+
+    }
+}
---
+++ Affirmations/app/src/main/java/com/antonioladeia/affirmations/models/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.antonioladeia.affirmations.models
+
+data class Affirmation(val stringResourceId: Int) {
+}
